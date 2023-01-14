42c209d1303d8bdd4c390159e9a400dff9e84378
Henrique Morais <rikeaju@hotmail.com>
2023-01-14 07:12:51 -0300

    Pirâmide de Testes

--- src/category/tests/unit/domain/test_unit_entities.py
+++ src/category/tests/unit/domain/test_unit_entities.py
@@ -1,11 +1,16 @@
+from category.domain.entities import Category
 from datetime import datetime
 import unittest
-from category.domain.entities import Category
 
-class TestCategory(unittest.TestCase):
+class TestCategoryUnit(unittest.TestCase):
     
     def test_constructor(self):
-        category = Category('Movie', 'some description', True, datetime.now())
+        category = Category(
+            name='Movie', 
+            description='some description', 
+            is_active=True, 
+            created_at=datetime.now()
+        )
         self.assertEqual(category.name, 'Movie')
         self.assertEqual(category.description, 'Some Description')
         self.assertEqual(category.is_active, True)
