2be32b473696e1bf59d18e4e1e40d581bc9f60af
Henrique Morais <rikeaju@hotmail.com>
2023-01-15 23:45:59 -0300

    Criando Primeiro Teste da Entidade Categoria com Unittest

---
+++ src/category/tests/domain/test_entities.py
@@ -0,0 +1,12 @@
+import unittest
+from datetime import datetime
+from category.domain.entities import Category
+
+class TestCategory(unittest.TestCase):
+    
+    def test_constructor(self):
+        category = Category('Movie', 'Some Description', True, datetime.now())
+        self.assertEqual(category.name, 'Movie')
+        self.assertEqual(category.description, 'Some Description')
+        self.assertEqual(category.is_active, True)
+        self.assertIsInstance(category.created_at, datetime)
