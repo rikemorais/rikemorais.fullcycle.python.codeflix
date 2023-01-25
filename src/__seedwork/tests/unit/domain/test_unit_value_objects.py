de23b174b1aa7071be6ec71ca7a8402d17d8388d
Henrique Morais <rikeaju@hotmail.com>
2023-01-25 12:38:54 +0000

    Teste do Objeto de Valor

--- src/__seedwork/tests/unit/domain/test_unit_value_objects.py
+++ src/__seedwork/tests/unit/domain/test_unit_value_objects.py
@@ -1,5 +1,8 @@
 import unittest
+import uuid
 from dataclasses import is_dataclass
+from unittest.mock import patch
+from __seedwork.domain.exceptions import InvalidUuidException
 from __seedwork.domain.value_objects import UniqueEntityId
 
 
@@ -7,3 +10,41 @@ class TestUniqueEntityIdUnit(unittest.TestCase):
     
     def test_if_is_a_dataclass(self):
         self.assertTrue(is_dataclass(UniqueEntityId))
+    
+    def test_throw_exception_when_uuid_is_invalid(self):
+        with patch.object(
+            UniqueEntityId,
+            '_UniqueEntityId__validate',
+            autospec=True,
+            side_effect=UniqueEntityId._UniqueEntityId__validate
+        ) as mock_validate:
+            with self.assertRaises(InvalidUuidException) as assert_error:
+                UniqueEntityId('fake id')
+            mock_validate.assert_called_once()
+            self.assertEqual(assert_error.exception.args[0], 'ID must be a valid UUID')
+    
+    def test_accept_uuid_passed_in_contructor(self):
+        with patch.object(
+            UniqueEntityId,
+            '_UniqueEntityId__validate',
+            autospec=True,
+            side_effect=UniqueEntityId._UniqueEntityId__validate
+        ) as mock_validate:
+            value_object = UniqueEntityId('930a1d4c-b59f-41c5-8068-f582186ca22b')
+            mock_validate.assert_called_once()
+            self.assertEqual(value_object.id, '930a1d4c-b59f-41c5-8068-f582186ca22b')
+            
+            uuid_value = uuid.uuid4()
+            value_object = UniqueEntityId(uuid_value)
+            self.assertEqual(value_object.id, str(uuid_value))
+    
+    def test_generate_id_when_no_passed_id_in_constructor(self):
+        with patch.object(
+            UniqueEntityId,
+            '_UniqueEntityId__validate',
+            autospec=True,
+            side_effect=UniqueEntityId._UniqueEntityId__validate
+        ) as mock_validate:
+            value_object = UniqueEntityId()
+            uuid.UUID(value_object.id)
+            mock_validate.assert_called_once()
